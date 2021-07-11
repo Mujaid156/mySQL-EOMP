3b575bde3b5cef8cc138c3c03c057cf8f2cf5266
Mujaid156 <mujaid.kariem22@gmail.com>
2021-07-11 11:38:11 +0200

    fifth commit

--- register.py
+++ register.py
@@ -1,20 +1,29 @@
 from tkinter import *
 import mysql.connector
 from tkinter import messagebox
+import tkinter as tk
 
-register = Tk()
+register = tk.Tk()
 register.title("Life Choices Online")
-register.geometry("800x500")
+register.geometry("800x600")
 register.config(bg="skyblue")
+
+name = StringVar()
+sur = StringVar()
+id = StringVar()
+cell = StringVar()
+kin_n = StringVar()
+kin_cell = StringVar()
+word = StringVar()
 
 mydb = mysql.connector.connect(host="localhost",user="lifechoices",password="@Lifechoices1234", auth_plugin='mysql_native_password',database="lifechoices")
 def into_database():
-    x = name_input.get()
-    y = surname_input.get()
-    z = id_input.get()
-    m = phone_input.get()
-    c = kin_name_input.get()
-    d = kin_phone_input.get()
+    x = name.get()
+    y = sur.get()
+    z = id.get()
+    m = cell.get()
+    c = kin_n.get()
+    d = kin_cell.get()
     if x == '' or y == '' or z == '' or m == '' or c == '' or d == '':
         messagebox.showinfo("Invalid Entry", "Please fillin all required feilds")
     else:
@@ -41,21 +50,23 @@ def exit_program():
 # Creating Labels
 visit = Label(register, text="Register", font="Arial 30 italic", bg="skyblue")
 user = Label(register, text="User Details", font="Arial 20 italic", bg="skyblue")
-name = Label(register, text="Name", bg="skyblue")
+names = Label(register, text="Name", bg="skyblue")
 surname = Label(register, text="Surname", bg="skyblue")
-id = Label(register, text="ID Number", bg="skyblue")
+id_num = Label(register, text="ID Number", bg="skyblue")
 phone = Label(register, text="Phone Number", bg="skyblue")
 kin = Label(register, text="Emergency Contact", font="Arial 20 italic", bg="skyblue")
 kin_name = Label(register, text="Name", bg="skyblue")
 kin_phone = Label(register, text="Phone Number", bg="skyblue")
+passw = Label(register, text="Password", bg="skyblue")
 
 # Creating Entries
-name_input = Entry(register)
-surname_input = Entry(register)
-id_input = Entry(register)
-phone_input = Entry(register)
-kin_name_input = Entry(register)
-kin_phone_input = Entry(register)
+name_input = Entry(register, textvariable=name)
+surname_input = Entry(register, textvariable=sur)
+id_input = Entry(register, textvariable=id)
+phone_input = Entry(register, textvariable=cell)
+kin_name_input = Entry(register, textvariable=kin_n)
+kin_phone_input = Entry(register, textvariable=kin_cell)
+passw_input = Entry(register, textvariable=word, show="*")
 
 # Creating Buttons
 clear = Button(register, text="Clear", command=clear)
@@ -63,9 +74,9 @@ exit = Button(register, text="Return to main page", command=exit_program)
 login = Button(register, text="Create User", command=into_database)
 
 # Placing Buttons
-clear.place(x=200, y=400)
-exit.place(x=450, y=400)
-login.place(x=300, y=400)
+clear.place(x=200, y=500)
+exit.place(x=450, y=500)
+login.place(x=300, y=500)
 
 # Placing Entries
 name_input.place(x=200, y=200)
@@ -74,14 +85,16 @@ id_input.place(x=200, y=300)
 phone_input.place(x=200, y=350)
 kin_name_input.place(x=550, y=200)
 kin_phone_input.place(x=550, y=250)
+passw_input.place(x=200, y=400)
 
 # Placing Labels
 visit.place(x=340, y=30)
 user.place(x=150, y=130)
-name.place(x=50, y=200)
+names.place(x=50, y=200)
 surname.place(x=50, y=250)
-id.place(x=50, y=300)
+id_num.place(x=50, y=300)
 phone.place(x=50, y=350)
+passw.place(x=50, y=400)
 kin.place(x=470, y=130)
 kin_name.place(x=420, y=200)
 kin_phone.place(x=420, y=250)
